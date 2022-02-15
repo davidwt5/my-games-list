07f2826a730dd2b129eb8540caf995bfa61778aa
davidwt5 <davidwtan75@gmail.com>
2022-02-15 16:43:06 +1100

    build gameCard UI

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,5 +1,24 @@
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+
+// No genre as of know, figure that out later. Genre does exist on giantbomb though
 function GameCard({ game }) {
-  return <p>{game.name}</p>;
+  const { name, image, deck, original_release_date } = game;
+  return (
+    <Card className="my-4 text-center" style={{ width: "100%" }}>
+      <Card.Header as="h5">{name}</Card.Header>
+      <Image
+        src={image.original_url}
+        className="w-75 my-4 m-auto"
+        style={{ aspectRatio: "4/5" }}
+      ></Image>
+      <Card.Body>
+        <Card.Text>{deck}</Card.Text>
+        <Card.Text>{original_release_date}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
 }
 
 export default GameCard;
