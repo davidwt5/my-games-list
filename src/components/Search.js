e7cbabd3f110c1ec44d3aaa65ae0df5e5b6df214
davidwt5 <davidwtan75@gmail.com>
2022-02-15 16:43:28 +1100

    created bootstrap grid layout

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,13 @@ import { useSearchParams } from "react-router-dom";
 import GameCard from "./GameCard";
 import config from "../config";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Search() {
   const [searchParams] = useSearchParams();
-  const [searchResult, setsearchResult] = useState(null);
+  const [searchResponse, setsearchResponse] = useState(null);
   const searchTitle = searchParams.get("title");
 
   function urlGenerator({ domain = "", endpoint = "", queryStrings = {} }) {
@@ -17,48 +21,52 @@ function Search() {
     return url;
   }
 
-  async function searchGames(title = "", offset = 0) {
-    if (title.length < 3) {
-      return Promise.reject(
-        new Error("search query must be at least 3 characters long")
-      );
-    }
-
-    const { giantBomb } = config;
-    const url = urlGenerator({
-      domain: "http://localhost:8080/",
-      endpoint: "giantbomb.com/api/search",
-      queryStrings: {
-        api_key: giantBomb.apiKey,
-        format: "json",
-        field_list: "id,name,image",
-        resources: "game",
-        query: title,
-        offset: offset,
-        limit: 12,
-      },
-    });
-    const response = await fetch(url);
-    return response.json();
-  }
-
   useEffect(() => {
-    searchGames(searchTitle)
-      .then((res) => setsearchResult(res))
-      .catch((e) => setsearchResult(e));
-  }, []);
+    async function searchGames(title = "", offset = 0) {
+      if (title.length < 3) {
+        return Promise.reject(
+          new Error("search query must be at least 3 characters long")
+        );
+      }
 
-  if (searchResult === null) {
-    return <p>loading</p>;
-  } else if (searchResult instanceof Error) {
-    return <p>{searchResult.message}</p>;
-  } else {
-    console.log(searchResult);
-    const gameCards = [];
-    for (let result of searchResult.results) {
-      gameCards.push(<GameCard key={result.id} game={result} />);
+      const { giantBomb } = config;
+      const url = urlGenerator({
+        domain: "http://localhost:8080/",
+        endpoint: "giantbomb.com/api/search",
+        queryStrings: {
+          api_key: giantBomb.apiKey,
+          format: "json",
+          // field_list: "id,name,image",
+          resources: "game",
+          query: title,
+          offset: offset,
+          limit: 12,
+        },
+      });
+      const response = await fetch(url);
+      return response.json();
     }
-    return <div>{gameCards}</div>;
+    searchGames(searchTitle)
+      .then((res) => setsearchResponse(res))
+      .catch((e) => setsearchResponse(e));
+  }, [searchTitle]);
+
+  if (searchResponse === null) {
+    return <p>loading</p>;
+  } else if (searchResponse instanceof Error) {
+    return <p>{searchResponse.message}</p>;
+  } else {
+    console.log(searchResponse);
+    const cells = searchResponse.results.map((result) => (
+      <Col xs={12} md={6} lg={4} key={result.id}>
+        <GameCard game={result} />
+      </Col>
+    ));
+    return (
+      <Container>
+        <Row>{cells}</Row>
+      </Container>
+    );
   }
 }
 
