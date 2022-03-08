0b088030ede27bd483de84a6d4ff49b351eb91cd
davidwt5 <davidwtan75@gmail.com>
2022-03-08 19:33:27 +1100

    dynamic rendering of first name

--- website/src/components/Header/LoggedInUser.js
+++ website/src/components/Header/LoggedInUser.js
@@ -2,18 +2,19 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 
-function LoggedInUser() {
+function LoggedInUser({ user }) {
+  const { firstName, profilePic } = user;
   return (
     <Nav className="ms-auto">
       <NavDropdown
         title={
           <div style={{ display: "inline" }}>
             <img
-              src={"/img/stock-profile.webp"}
+              src={profilePic ? profilePic : "/img/stock-profile.webp"}
               alt="user pic"
               style={{ height: 50, marginRight: 10 }}
             />
-            {"John Doe"}
+            {firstName}
           </div>
         }
         id="user-dropdown"
@@ -25,7 +26,9 @@ function LoggedInUser() {
           <NavDropdown.Item>My Games</NavDropdown.Item>
         </LinkContainer>
         <NavDropdown.Divider />
-        <NavDropdown.Item onClick={() => alert("signed out")}>Sign Out</NavDropdown.Item>
+        <NavDropdown.Item onClick={() => alert("signed out")}>
+          Sign Out
+        </NavDropdown.Item>
       </NavDropdown>
     </Nav>
   );
