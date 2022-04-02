39adc2d4e3d5c44e3e35562321cf6b5129262b26
davidwt5 <davidwtan75@gmail.com>
2022-04-02 19:41:48 +1100

    extracted giantbomb api call to server; no need for cors proxy server anymore

--- server/app.js
+++ server/app.js
@@ -168,8 +168,29 @@ app.get("/gameslist", (req, res) => {
   })();
 });
 
+// No need to authenticate user for search
+// Verification on input length, etc. Sanitise user input for attacks?
 app.get("/searchgames", (req, res) => {
+  const title = req.query.title;
+  (async () => {
+    const url = urlGenerator({
+      domain: "http://giantbomb.com/",
+      endpoint: "api/search",
+      queryStrings: {
+        api_key: config.giantBomb.apiKey,
+        format: "json",
+        // field_list: "id,name,image",
+        resources: "game",
+        query: title,
+        offset: 0,
+        limit: 12,
+      },
+    });
 
+    const response = await fetch(url);
+    const result = await response.json();
+    res.json(result);
+  })();
 });
 
 app.get("/cookietest", (req, res) => {
--- website/src/components/PageBodies/Search.js
+++ website/src/components/PageBodies/Search.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import GameCard from "../Utility/GameCard";
-import config from "../../config";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -12,15 +11,7 @@ function Search() {
   const [searchResponse, setsearchResponse] = useState(null);
   const searchTitle = searchParams.get("title");
 
-  function urlGenerator({ domain = "", endpoint = "", queryStrings = {} }) {
-    if (!domain) return "";
-    let url = new URL(endpoint, domain);
-    for (let key of Object.keys(queryStrings)) {
-      url.searchParams.append(key, queryStrings[key]);
-    }
-    return url;
-  }
-
+  // Refetches data whenever the searchTitle changes
   useEffect(() => {
     // Should go through backend instead of hitting API directly
     async function searchGames(title = "", offset = 0) {
@@ -29,23 +20,9 @@ function Search() {
           new Error("search query must be at least 3 characters long")
         );
       }
-
-      const { giantBomb } = config;
-      const url = urlGenerator({
-        domain: "http://localhost:8080/",
-        endpoint: "giantbomb.com/api/search",
-        queryStrings: {
-          api_key: giantBomb.apiKey,
-          format: "json",
-          // field_list: "id,name,image",
-          resources: "game",
-          query: title,
-          offset: offset,
-          limit: 12,
-        },
-      });
-      const response = await fetch(url);
-      return response.json();
+      const url = "http://localhost:4000/searchgames"
+      const response = await fetch(`${url}?title=${title}`);
+      return await response.json();
     }
     searchGames(searchTitle)
       .then((res) => setsearchResponse(res))
