2714eb83b8b488d00c5a5422844e0c835ca384bf
davidwt5 <davidwtan75@gmail.com>
2022-04-03 18:34:55 +1000

    comment

--- server/src/authentication/authenticate.js
+++ server/src/authentication/authenticate.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 
+// If the user is found in db then put that user data in req
 async function authenticate(req, res, next) {
   const { sessionId, username } = req.cookies;
   const user = await User.findOne({ sessionId, username });
