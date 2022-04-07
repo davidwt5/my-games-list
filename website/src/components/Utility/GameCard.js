0afb6e394798fc7c2b0a2e375a9e13b057d5c465
davidwt5 <davidwtan75@gmail.com>
2022-04-07 16:29:37 +1000

    modularised a component

--- website/src/components/Utility/GameCard.js
+++ website/src/components/Utility/GameCard.js
@@ -1,63 +1,9 @@
-import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import { Plus, Minus, List } from "react-feather";
 import "./GameCard.css";
+import GamelistControls from "./GamelistControls";
 
-async function addGame(e) {
-  const gameId = e.target.dataset.id;
-  const addGameURL = "http://localhost:4000/gameslist";
-  try {
-    const response = await fetch(addGameURL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        gameId: gameId,
-      }),
-      credentials: "include", // Security risk? Need this so the browser sends cookies.
-    });
-    if (response.status !== 200) {
-      console.log("ERROR: " + response.status);
-    } else {
-      alert("successfully added game");
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-async function removeGame(e) {
-  const gameId = e.target.dataset.id;
-  const addGameURL = `http://localhost:4000/gameslist/${gameId}`;
-  try {
-    const response = await fetch(addGameURL, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include", // Security risk? Need this so the browser sends cookies.
-    });
-    if (response.status !== 200) {
-      console.log("ERROR: " + response.status);
-    } else {
-      alert("successfully removed game");
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-// Consider a heart and unheart system instead
 function GameCard({ game }) {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   const { id, name, image, deck, original_release_date } = game;
   return (
     <Card className="my-4 text-center" style={{ width: "100%" }}>
@@ -72,40 +18,8 @@ function GameCard({ game }) {
         <Card.Text>{original_release_date}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">
-        <Plus
-          data-id={id}
-          stroke-width={1.5}
-          className="card-footer-icon mx-1"
-          onClick={addGame}
-        />
-        <List
-          data-id={id}
-          stroke-width={1.5}
-          className="card-footer-icon mx-1"
-          onClick={handleShow}
-        />
-        <Minus
-          data-id={id}
-          stroke-width={1.5}
-          className="card-footer-icon mx-1"
-          onClick={removeGame}
-        />
+        <GamelistControls id={id} />
       </Card.Footer>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </Card>
   );
 }
---
+++ website/src/components/Utility/GamelistControls.js
@@ -0,0 +1,99 @@
+/* A set of controls to add, remove, etc. A specific game from a user's gameslist
+ Consider a heart and unheart system instead
+ */
+
+import { useState } from "react";
+import { Plus, Minus, List } from "react-feather";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+async function addGame(e) {
+  const gameId = e.target.dataset.id;
+  const addGameURL = "http://localhost:4000/gameslist";
+  try {
+    const response = await fetch(addGameURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        gameId: gameId,
+      }),
+      credentials: "include", // Security risk? Need this so the browser sends cookies.
+    });
+    if (response.status !== 200) {
+      console.log("ERROR: " + response.status);
+    } else {
+      alert("successfully added game");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function removeGame(e) {
+  const gameId = e.target.dataset.id;
+  const addGameURL = `http://localhost:4000/gameslist/${gameId}`;
+  try {
+    const response = await fetch(addGameURL, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Security risk? Need this so the browser sends cookies.
+    });
+    if (response.status !== 200) {
+      console.log("ERROR: " + response.status);
+    } else {
+      alert("successfully removed game");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Takes the id of the target game
+function GamelistControls({ id }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div>
+      <Plus
+        data-id={id}
+        stroke-width={1.5}
+        className="card-footer-icon mx-1"
+        onClick={addGame}
+      />
+      <List
+        data-id={id}
+        stroke-width={1.5}
+        className="card-footer-icon mx-1"
+        onClick={handleShow}
+      />
+      <Minus
+        data-id={id}
+        stroke-width={1.5}
+        className="card-footer-icon mx-1"
+        onClick={removeGame}
+      />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default GamelistControls;
