425dd631224859fd99cc58341ac09bf39aa4e3ad
davidwt5 <davidwtan75@gmail.com>
2022-04-14 12:57:48 +1000

    margin and padding

--- website/src/components/Utility/Loading.js
+++ website/src/components/Utility/Loading.js
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 
 function Loading() {
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center p-5 m-5">
+    <Container className="d-flex flex-column justify-content-center align-items-center py-4 my-4">
       <Spinner animation="border" variant="primary" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
