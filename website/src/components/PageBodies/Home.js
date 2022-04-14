a02f3f0eb0ba13b3afe104191c999f6099b438a1
davidwt5 <davidwtan75@gmail.com>
2022-04-14 11:11:03 +1000

    home page structure

--- website/src/components/PageBodies/Home.js
+++ website/src/components/PageBodies/Home.js
@@ -1,8 +1,15 @@
+import Container from "react-bootstrap/Container";
+
 function Home() {
   return (
-    <div className="home">
-      <h1>Home</h1>
-    </div>
+    <Container className="d-flex flex-column p-5">
+      <h1 className="display-4" style={{width: "100%"}}>
+        Welcome! <hr />
+      </h1>
+      <h3 className="lead">
+        This is my first full stack personal project where...
+      </h3>
+    </Container>
   );
 }
 
